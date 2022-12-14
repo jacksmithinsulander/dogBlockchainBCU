2ddb8646ce6ef5a1f00ecf594e26ce9bd1d5c1f1
martinsjoeborg <martinsjoeborg@gmail.com>
2022-12-14 14:41:01 +0100

    uppd 2

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,43 @@ validateBtn.addEventListener("click", () => {
     timeSheet.isChainValid();
 })
 
+//FETCH
+export default function printPokemonData(pokemonUrl) {
+
+    fetch(pokemonUrl)
+
+    .then(res => res.json())
+
+    .then(data => {
+
+        //console.log("pokemon data", data);
+
+        poke_data.innerHTML = "<h3>" + data.name + "</h3>";
+
+       
+
+        data.moves.map(move => {
+
+            //console.log("move", move.move)
+
+            poke_data.insertAdjacentHTML("beforeend", " - " + move.move.name )
+
+        });
+
+
+
+        let pokemonImage = document.createElement("img");
+
+        pokemonImage.src = data.sprites.front_default
+
+
+
+        poke_data.appendChild(pokemonImage);
+
+    })
+
+}
+
 editBtn.addEventListener("click", () => {
   //  console.log("Im going to edit something!");
     timeSheet.timeSheet[2].data.work = 20;
