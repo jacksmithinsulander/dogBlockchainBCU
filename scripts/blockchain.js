1fee3614315c8ee7fbfdc56dc0dbc7a12316d059
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-12-20 18:30:47 +0100

    made the blockchain explorer fancier

--- scripts/blockchain.js
+++ scripts/blockchain.js
@@ -7,12 +7,6 @@ import chainGet from "./chain_revitalizer.js";
 
 export default function blockchain () {
 
-    //let newBlock = new Chain();
-
-    // Gör en if else funktion som kollar ifall en blockkedja finns i LS eller ej, om den finns så laddain den förexisterande, 
-    // annars så kan den göra så som den gör nu (alltså printar genesisblock). Försök även baka in i denna funktionen
-    // en variabel som börjar på 1 och varje gång ett block adderas  så ++ar den. Tänker att denna aggerar som blockidentifierare i kedjan
-    // Så om blockkedjan finns i ls så ska denna siffra också läsas ifrån LS. 
     let content = document.getElementById("content");
 
     const chooseDogText = document.createElement("p");
@@ -35,14 +29,10 @@ export default function blockchain () {
         var blockArray = new Chain();
         localStorage.setItem('blockchainObjectArr', JSON.stringify(blockArray));
         printBlockChain();
-        //console.log('ls has been created');
     } else {
-        //console.log('ls is updating');
         var blockArray = JSON.parse(localStorage.getItem('blockchainObjectArr'));
         blockArray = new chainGet(blockArray);
-        //console.log(blockArray);
         printBlockChain();
-        //console.log('ls has been updated');
     } 
 
     fetch('https://dog.ceo/api/breeds/list/all') 
@@ -59,7 +49,6 @@ export default function blockchain () {
     }
 
     setTimeout(() => {
-        //console.log(dogsArray);
         makeDropDown();
     }, 500);
 
@@ -70,30 +59,29 @@ export default function blockchain () {
         return json[0]
     }
 
-    //När vi gör blocket så kan vi även använda detta api: https://dog.ceo/api/breed/HUNDRAS/images/random (exempel: https://dog.ceo/api/breed/akita/images/random)
-    // för att även lägga till en url till en random bild i själva blocket som vi kan använda för att rendera i bild på hunden i t.ex. explorern eller ens 
-    // dogwallet (som absolut är något vi borde lägga till!
+    async function setDogImg(choosenDogBreed) {
+        let baseURL = "https://dog.ceo/api/breed/";
+        let URLEnding = "/images/random";
+        const urlResponse = await fetch(baseURL.concat(choosenDogBreed, URLEnding))
+        const urlJSON = await urlResponse.json()
+        return urlJSON.message;
+        }
+    
  function buyDog() { 
-    //console.log("köp " + dropDown.value);
         (async () => {
+            const dogImg = await setDogImg(dropDown.value);
             const dogsName = await setDogName()
             let blockData = { 
                 timeStamp: Math.floor(Date.now() / 1000),
                 dogBreed: dropDown.value,
                 dogName: dogsName,
+                dogImage: dogImg,
                 dogXP: 0,
                 dogState: "With owner"
             }
-            //console.log(blockData);
-            //console.log(blockArray);
             blockArray.addTime(new Block(blockData));
             console.log(blockArray);
-
             localStorage.setItem("blockchainObjectArr", JSON.stringify(blockArray))
-
-            //blockArray.addTime(new Block(blockData));
-            //localStorage.setItem("blockchainObjectArr", JSON.stringify(blockArray))
-
         printBlockChain();
         })();
  }
@@ -101,38 +89,22 @@ export default function blockchain () {
         buyDog();
     }); 
 
-    //I och med att jag ändrade  här ovanför igår (funktionen write block) så att vi får ett ganska korrekt objekt i ls (vi får väll 
-    // fixa till det objektet med så vi får in all info som vi vill ha sen, hehe) så får vi göra om brint chain funktionen. 
-    // Gör om den så att den istället för att bygga ihop det printade blocket som den gör manuellt nu, så läser den det från blockkedjan från ls. 
-
     async function printBlockChain() {
-        //console.log("printing chain");
         blockExplorer.innerHTML = ""; 
-        //for (block in blockArray) {
             blockArray.timeSheet.map(work => {
-                //console.log("printing chain");
-                //setTimeout(console.log("print new"), 1000)
-                //console.log(work);
                 let timeBox = document.createElement("div");
                 timeBox.style.border = "3px solid magenta";
                 timeBox.style.padding = "20px";
                 timeBox.style.margin = "20px";
                 timeBox.style.backgroundColor = "cyan"
                 timeBox.style.borderRadius = "10px"
-
-                //timeBox.innerHTML = "<p>" + work.prevHash + "<br/>" + work.data.dogBreed + "<br/>" + work.data.dogName + "<br/>" + work.hash + "</p>"
-
                 timeBox.id = work.id;
-
                 if (work.data.dogName === "Genesis"){
                     timeBox.innerHTML = "<p> Genesis block </p>"
                 }else {
-                    timeBox.innerHTML = "<p> Previous Hash: " + work.prevHash + "<br/> Dogbreed: " + work.data.dogBreed + "<br/> Dogs Name: " + work.data.dogName + "<br> Dogs XP: " + work.data.dogXP + "<br> Dog is: " + work.data.dogState + "<br> Timestamp: " + work.data.timeStamp  + "<br/> Hash: " + work.hash + "</p>"
+                    timeBox.innerHTML = "<p> Previous Hash: " + work.prevHash + "<br/> Dogbreed: " + work.data.dogBreed + "<br/> Dogs Name: " + work.data.dogName + "<br> Dogs XP: " + work.data.dogXP + "<br> Dog is: " + work.data.dogState + "<br> Dog Image URL: " + work.data.dogImage + "<br> Timestamp: " + work.data.timeStamp  + "<br/> Hash: " + work.hash + "</p> <br> <img src='" + work.data.dogImage + "' style='object-fit:contain;'>" 
                 }
-
-                //timeBox.innerHTML = "<p>" + blockArray[block] + "</p>";
                 blockExplorer.appendChild(timeBox);
             })
-        //}
     }
 }
