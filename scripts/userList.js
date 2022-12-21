fb4a8b84812541656103d0618c97f89041ad5f20
gitmazter <philip.andelic@gmail.com>
2022-12-21 19:52:37 +0700

    213

--- scripts/userList.js
+++ scripts/userList.js
@@ -3,7 +3,7 @@ export default class UserList {
         console.log(memberData);
         this.users = []; // Remake user objects // return empty array if member data is empty
     }
-
+    // its an object that holds an array that contains objects. 
     addUser(user) {
         this.users.push(user);
         //user.addToLs();
